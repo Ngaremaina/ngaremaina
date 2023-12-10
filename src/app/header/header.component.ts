316c2fde257b50518568ce44dcfac96037a9c73c
Ngaremaina <ngaremaina4@gmail.com>
2023-12-10 16:50:23 +0300

    :lipstick: Portfolio Structure

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  divIds: string[] = ['Contact', 'About', 'Home']; // IDs of the divs to scroll to
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  scrollToElement(id: string): void {
+    const element = this.el.nativeElement.querySelector(`#${id}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+  }
+
+}
