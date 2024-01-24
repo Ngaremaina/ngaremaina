c63687c09a29e4fb4cec14c2741d26e96e592258
Ngaremaina <ngaremaina4@gmail.com>
2024-01-24 14:49:46 +0300

    Update data.js

--- src/Components/data.js
+++ src/Components/data.js
@@ -18,7 +18,7 @@ export const projects = [
         name:"Mapishi",
         description:"Mapishi provide cooks at home with recipes that will assist them to live a healthier and happier life for themselves, their communities, and the world. The software enables the user to look for recipes and examine information about them, including their ingredients.",
         link:"https://mapishi.netlify.app/",
-        image:"assets/img/lakucha dishes.jpg"
+        image:"https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
     },
     {
