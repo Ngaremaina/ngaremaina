ac6d15abd28e22c7a44bf5e9da238ac73f512277
Ngaremaina <ngaremaina4@gmail.com>
2024-01-24 14:34:04 +0300

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,12 +3,12 @@ import { projects } from './data'
 
 const Projects = () => {
     const displayProjects = projects.map((project) => {
-        return <div className='col-lg-4 col-md-6 portfolio-item'><div className="portfolio-wrap"><img src={project.image} className="img-fluid" style={{height:'300px'}} alt = {project.name} /><div className="portfolio-links"><a href={project.image} data-gallery="portfolioGallery" className="portfolio-lightbox" title={project.name}><i className="bx bx-plus" /></a><a href={project.link} title="More Details"><i className="bx bx-link" /></a></div></div></div>
+        return <div className='col-lg-4 col-md-6 projects-item'><div className="projects-wrap"><img src={project.image} className="img-fluid" style={{height:'300px'}} alt = {project.name} /><div className="projects-links"><a href={project.image} data-gallery="projectsGallery" className="projects-lightbox" title={project.name}><i className="bx bx-plus" /></a><a href={project.link} title="More Details"><i className="bx bx-link" /></a></div></div></div>
 
     })
     return(
         
-        <Element name="projects" className="portfolio section-bg">
+        <Element name="projects" className="projects section-bg">
         <div className="container">
             <div className="section-title">
             <h2>Personal Projects</h2>
@@ -16,7 +16,7 @@ const Projects = () => {
             </div>
             {/* <div className="row" data-aos="fade-up">
             <div className="col-lg-12 d-flex justify-content-center">
-                <ul id="portfolio-flters">
+                <ul id="projects-flters">
                 <li data-filter="*" className="filter-active">All</li>
                 <li data-filter=".filter-app">App</li>
                 <li data-filter=".filter-card">Card</li>
@@ -24,7 +24,7 @@ const Projects = () => {
                 </ul>
             </div>
             </div> */}
-            <div className="row portfolio-container" data-aos="fade-up" data-aos-delay={100}>
+            <div className="row projects-container" data-aos="fade-up" data-aos-delay={100}>
                 {displayProjects}
             </div>
         </div>
