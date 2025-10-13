72a7ddebb69abae4f94338796ebadfee40e65231
Ngare Maina <45742522+Ngaremaina@users.noreply.github.com>
2025-10-13 18:19:39 +0300

    💄 Button UI - Changed color of Submit Button from gray to blue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,7 +88,7 @@ const Contact = () => {
               <label htmlFor="message" className="mb-2 text-sm font-medium text-white">Your message</label>
               <textarea id="message" rows="4" className="mb-3 block p-2.5 w-full text-sm text-black rounded-lg" placeholder="Leave a comment..." value={form.message} onChange={handleChange}></textarea>
 
-              <button type="submit" className="text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2">Submit</button>
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
         </div>
         <div className="p-4 rounded-lg text-white alternate-background">
