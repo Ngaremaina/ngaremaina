4c1ef8ad7201722114847b1f10638bf20424bcda
Ngare Maina <45742522+Ngaremaina@users.noreply.github.com>
2025-10-13 11:51:06 +0000

    :sparkles: Vercel Analytics - Added Vercel Analytics into the app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Background from './Components/Background';
 import Contact from './Components/Contact';
 import Skills from './Components/Skills';
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [isActive, setActive] = useState(false)
@@ -19,6 +20,7 @@ function App() {
   
   return (
     <div className={className ? 'mobile-nav-active':''}>
+    <Analytics/>
       <i className={`d-xl-none bi mobile-nav-toggle ${isActive ? 'bi-x' : 'bi-list'}`} onClick={handleNav}/>
       <NavBar/>
       <main id = 'main'>
